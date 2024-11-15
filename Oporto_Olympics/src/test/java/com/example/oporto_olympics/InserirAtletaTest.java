0472b3b034d096714ac75d23e22bc91151cfd30a
Eduardo_Pinto <1221517@isep.ipp.pt>
2024-11-15 21:01:48 +0000

    Teste unitário de inserir atleta atualizado

--- Oporto_Olympics/src/test/java/com/example/oporto_olympics/InserirAtletaTest.java
+++ Oporto_Olympics/src/test/java/com/example/oporto_olympics/InserirAtletaTest.java
@@ -2,58 +2,28 @@ package com.example.oporto_olympics;
 
 import com.example.oporto_olympics.Controllers.Atleta.InserirAtletaController;
 import com.example.oporto_olympics.DAO.Atleta.InserirAtletaDAOImp;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
-import static javafx.beans.binding.Bindings.when;
-import static org.mockito.Mockito.clearInvocations;
+import java.sql.Connection;
+import java.time.LocalDate;
 
 class InserirAtletaTest {
 
-    private InserirAtletaController controller;
-
-    @Mock
-    private InserirAtletaDAOImp dao;
-
-    @Mock
-    private TextField Nome;
-
-    @Mock
-    private TextField Pais;
-
-    @Mock
-    private TextField Altura;
-
-    @Mock
-    private TextField Peso;
-
-    @Mock
-    private TextField Data_nasc;
-
-    @Mock
-    private ChoiceBox<String> GeneroChoice;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
-
-    @AfterEach
-    void tearDown() {
-        // Limpar mocks após cada teste (opcional se for configurado automaticamente)
-        clearInvocations(dao, Nome, Pais, Altura, Peso, Data_nasc, GeneroChoice);
-    }
-
     @Test
     void onClickCriarAtletaButton() {
+        ConnectionBD connectionBD = ConnectionBD.getInstance();
+        Connection conexao = connectionBD.getConexao();
 
-        controller.OnClickCriarAtletaButton();
+        String nome = "Roberto Rodriguez";
+        String pais = "PRT";
+        Double altura = 1.75;
+        Double peso = 70.5;
+        LocalDate DataNasc = LocalDate.of(1998, 11, 25);
+        String genero = "Men";
+
+        InserirAtletaDAOImp dao = new InserirAtletaDAOImp(conexao);
+
+        dao.saveAtleta(nome,pais,altura,peso,DataNasc,genero);
+
     }
 }
