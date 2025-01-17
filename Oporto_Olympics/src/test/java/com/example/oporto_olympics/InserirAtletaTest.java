4df234427fdf05a328ae8eff3200009bf48ee6a5
Eduardo_Pinto <1221517@isep.ipp.pt>
2025-01-17 18:18:51 +0000

    Teste Unitário Inserir atleta atualizado

--- Oporto_Olympics/src/test/java/com/example/oporto_olympics/InserirAtletaTest.java
+++ Oporto_Olympics/src/test/java/com/example/oporto_olympics/InserirAtletaTest.java
@@ -15,52 +15,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InserirAtletaTest {
 
-    /**
-     * Testa a funcionalidade de inserir um novo atleta na base de dados.
-     *
-     * O teste verifica o seguinte:
-     * <ul>
-     *   <li>Insere um atleta com os dados fornecidos utilizando o DAO responsável.</li>
-     *   <li>Recupera todos os atletas da base de dados e verifica se o atleta inserido está presente.</li>
-     * </ul>
-     *
-     * Asserção:
-     * <ul>
-     *   <li>O teste confirma que o atleta inserido foi corretamente adicionado, verificando pelo nome.</li>
-     * </ul>
-     *
-     * @throws SQLException se ocorrer um erro de comunicação com a base de dados durante a execução.
-     */
-    @Test
-    void InserirAtleta() throws SQLException {
-        ConnectionBD connectionBD = ConnectionBD.getInstance();
-        Connection conexao = connectionBD.getConexao();
-        Boolean igual = false;
-
-        String nome = "Atleta test2";
-        String pais = "PRT";
-        Double altura = 1.80;
-        Double peso = 66.7;
-        LocalDate DataNasc = LocalDate.of(1998, 8, 8);
-        String genero = "Men";
-
-        InserirAtletaDAOImp dao = new InserirAtletaDAOImp(conexao);
-        AtletaDAOImp dao2 = new AtletaDAOImp(conexao);
-
-        dao.saveAtleta(nome,pais,altura,peso,DataNasc,genero);
-
-        for (Atleta atleta : dao2.getAll()){
-
-            if(atleta.getNome().equals(nome)){
-                igual = true;
-                break;
-            }
-
-        }
-
-        assertTrue(igual, "O atleta deveria existir..");
-    }
-
     @Test
     void InserirAtletaValido() throws SQLException {
         ConnectionBD connectionBD = ConnectionBD.getInstance();
